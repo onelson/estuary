ca6cd97dab97ae2f368a70b6a8cd4a92e03a9917
Owen Nelson <onelson@gmail.com>
2020-11-26 20:06:13 -0800

    Add functions for bootstrapping the registry repo and updating config.
    There's some juggling required to ensure the git repo is present and
    ready, then to ensure we don't write redundant revisions to the history.
    
    These functions let us:
    
    - create the git repo if needed
    - attempt to read an existing config.json
    - compare the preexisting config to the config specified in our program
    - update the config in the repo if they are different
    
    This should be most of the cases to handle when the web service starts up
    each time.

--- src/index.rs
+++ src/index.rs
@@ -1,7 +1,18 @@
 //! Index management.
+//!
+//! The registry's index is comprised of a config file and one file per crate.
+//! Each crate file contains a json object per line per published version.
+//!
+//! This "registry index" should not be confused with the "git index" which is
+//! also a thing in here since all changes to the registry index must be
+//! committed a the git repo which is also managed by this module..
 
+use anyhow::Result;
+use git2::{Oid, Repository, Signature};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::io::Write;
+use std::path::Path;
 
 /// The config.json for the registry.
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
@@ -99,15 +110,128 @@ enum DependencyKind {
 /// - Prevent differences of - vs _.
 /// - Under a specific length (max 64).
 /// - Rejects reserved names, such as Windows special filenames like "nul".
-#[allow(dead_code)]
-fn validate_package_name() -> Result<(), ()> {
+fn validate_package_name() -> Result<()> {
     todo!()
+}
+
+/// Get a git signature for "the system".
+fn get_sig() -> Result<Signature<'static>> {
+    Ok(Signature::now("estuary", "admin@localhost")?)
+}
+
+fn get_or_create_repo<P>(root: P) -> Result<Repository>
+where
+    P: AsRef<Path>,
+{
+    let sig = get_sig()?;
+    let root = root.as_ref();
+    let is_empty = std::fs::read_dir(root)?.next().is_none();
+    if is_empty {
+        log::debug!("Creating a fresh index.");
+        let repo = Repository::init(root)?;
+        {
+            let tree_id = {
+                let mut index = repo.index()?;
+                index.write_tree()?
+            };
+            let tree = repo.find_tree(tree_id)?;
+            repo.commit(Some("HEAD"), &sig, &sig, "init empty repo", &tree, &[])?;
+        }
+        Ok(repo)
+    } else {
+        log::debug!("Using preexisting index.");
+        Ok(Repository::open(root)?)
+    }
+}
+
+/// Initialize a fresh (registry) index.
+///
+/// Given an empty directory, this will create a new git repo containing a
+/// `config.json`.
+///
+/// If the directory is non-empty *and has a git repo in it*, the assumption is
+/// there's already a valid index at that path.
+/// An attempt to update the config (if necessary) using the supplied values
+/// will be made.
+fn init<P>(root: P, config: &Config) -> Result<()>
+where
+    P: AsRef<Path>,
+{
+    let root = root.as_ref();
+    let repo = get_or_create_repo(root)?;
+    let current_config: Option<Config> = read_config_from_disk(root).ok();
+
+    if Some(config) != current_config.as_ref() {
+        write_config_to_disk(root, config)?;
+
+        let head = repo.head()?;
+        let parent = head.peel_to_commit()?;
+
+        let mut index = repo.index()?;
+
+        index.add_path(Path::new("config.json"))?;
+        index.write()?;
+
+        let tree_id = index.write_tree()?;
+        let tree = repo.find_tree(tree_id)?;
+        let sig = get_sig()?;
+        repo.commit(
+            Some("HEAD"),
+            &sig,
+            &sig,
+            "update registry config",
+            &tree,
+            &[&parent],
+        )?;
+    }
+
+    Ok(())
+}
+
+/// Read and parse the config file from the registry root directory.
+fn read_config_from_disk<P>(root: P) -> Result<Config>
+where
+    P: AsRef<Path>,
+{
+    let fh = std::fs::File::open(root.as_ref().join("config.json"))?;
+    Ok(serde_json::from_reader(fh)?)
+}
+
+/// Write the config to the registry root directory.
+fn write_config_to_disk<P>(root: P, config: &Config) -> Result<()>
+where
+    P: AsRef<Path>,
+{
+    log::debug!("Writing registry config file.");
+    let mut fh = std::fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .append(false)
+        .open(root.as_ref().join("config.json"))?;
+    fh.write_all(&serde_json::to_vec(config)?)?;
+    fh.flush()?;
+    fh.sync_all()?;
+    Ok(())
+}
+
+fn get_repo_log<P>(root: P) -> Result<Vec<(Oid, Option<String>)>>
+where
+    P: AsRef<Path>,
+{
+    let repo = get_or_create_repo(root.as_ref())?;
+    Ok(repo
+        .reflog("HEAD")?
+        .iter()
+        .map(|entry| (entry.id_new(), entry.message().map(String::from)))
+        .collect())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use serde_json::json;
+    use tempdir::TempDir;
 
     /// Parse the sample object from the cargo docs to verify our structs
     /// capture all the keys they're supposed to.
@@ -160,5 +284,98 @@ mod tests {
         );
         assert_eq!(false, pkg.yanked);
         assert_eq!(None, pkg.links);
+    }
+
+    #[test]
+    fn test_init_empty_dir() {
+        let root = TempDir::new("test_empty").unwrap();
+
+        init(
+            &root,
+            &Config {
+                dl: String::from("http://localhost/dl"),
+                api: String::from("http://localhost/api"),
+            },
+        )
+        .unwrap();
+        let entries = get_repo_log(&root).unwrap();
+
+        // There should be one commit for the empty repo, and one for the config
+        // update.
+        assert_eq!(entries.len(), 2);
+
+        assert_eq!(
+            entries
+                .into_iter()
+                .filter_map(|(_, msg)| msg)
+                .filter(|msg| msg.contains("update registry config"))
+                .count(),
+            1
+        );
+    }
+
+    #[test]
+    fn test_config_change_updates_repo() {
+        let root = TempDir::new("test_config_change_updates").unwrap();
+
+        init(
+            &root,
+            &Config {
+                dl: String::from("http://localhost/dl"),
+                api: String::from("http://localhost/api"),
+            },
+        )
+        .unwrap();
+
+        init(
+            &root,
+            &Config {
+                dl: String::from("http://example.com/dl"),
+                api: String::from("http://example.com/api"),
+            },
+        )
+        .unwrap();
+
+        let entries = get_repo_log(&root).unwrap();
+
+        // There should be one commit for the empty repo, and two for the config
+        // updates.
+        assert_eq!(entries.len(), 3);
+
+        assert_eq!(
+            entries
+                .into_iter()
+                .filter_map(|(_, msg)| msg)
+                .filter(|msg| msg.contains("update registry config"))
+                .count(),
+            2
+        );
+    }
+
+    #[test]
+    fn test_unchanged_config_does_not_update_repo() {
+        let root = TempDir::new("test_unchanged_config").unwrap();
+
+        let config = Config {
+            dl: String::from("http://localhost/dl"),
+            api: String::from("http://localhost/api"),
+        };
+
+        init(&root, &config).unwrap();
+        init(&root, &config).unwrap();
+        let entries = get_repo_log(&root).unwrap();
+
+        // There should be one commit for the empty repo, and one (only one) for
+        // the config update.
+        assert_eq!(entries.len(), 2);
+
+        assert_eq!(
+            entries
+                .into_iter()
+                .filter_map(|(_, msg)| msg)
+                .filter(|msg| msg.contains("update registry config"))
+                .count(),
+            1
+        );
     }
 }
